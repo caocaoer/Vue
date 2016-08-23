2f0514ea799ed08d9abd6ff81af7e0e4a7b6e811
caocaoer <caocaoer1019@163.com>
2016-08-23 18:56:43 +0800

    add

---
+++ vue-tree/js/data.js
@@ -0,0 +1,28 @@
+var data = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
+  ]
+};
