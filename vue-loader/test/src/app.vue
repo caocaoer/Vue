13d30385fb4f78f7805e8eec58c5cbc588009a89
caocaoer <caocaoer1019@163.com>
2016-12-02 10:33:20 +0800

    add

---
+++ vue-loader/test/src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>xxaaa</div>
+    <div>{{msg}}</div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                msg: 'hello world'
+            }
+        }
+    }
+</script>
+
+<style>
+    div{
+        color: red;
+    }
+</style>
---
+++ vue-loader/test/src/temp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>vue2</div>
+    <div>{{msg}}</div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                msg: 'hello world'
+            }
+        }
+    }
+</script>
+
+<style>
+    div{
+        color: red;
+    }
+</style>
